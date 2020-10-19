7c2db4113c5040fe29b9a7aa0142ff7d65edb2a4
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-10-19 12:10:28 +0600

    first commit Hello world

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+root = Tk()
+
+# Creating a Label widget
+
+appName = Label(root, text="Hawk Eye")
+myLabel = Label(root, text="Login")
+
+# Moving it into screen
+appName.pack()
+myLabel.pack()
+
+root.mainloop()
+
+
+
+
+
+
+
