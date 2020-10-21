195ac40c042b3417b40a670df2bdad4b62bccaba
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-10-21 23:26:34 +0600

    icon add in login label

--- login.py
+++ login.py
@@ -14,8 +14,8 @@ class LoginSystem:
 
         # ==============  All Images ==============
         self.bg_icon = ImageTk.PhotoImage(file="images/bg.jpg")
-        # self.user_icon = ImageTk.PhotoImage(file="")
-        # self.pass_icon = ImageTk.PhotoImage(file="")
+        self.user_icon = ImageTk.PhotoImage(file="images/icons8-user-male-26.png")
+        self.pass_icon = ImageTk.PhotoImage(file="images/icons8-forgot-password-26.png")
         self.placeholder_icon = ImageTk.PhotoImage(file="images/placeholder.png")
         bg_lbl = Label(self.root, image=self.bg_icon).pack()
         title = Label(self.root, text="Login System", font=("times new roman", 30, 'bold'), bg='yellow', fg='red', bd=10, relief=GROOVE)
@@ -26,10 +26,10 @@ class LoginSystem:
 
         logo_label = Label(login_frame, image=self.placeholder_icon, heigh=100, width=100).grid(row=0, columnspan=2, pady=20)
 
-        user_name_label = Label(login_frame, text= "Username", compound=LEFT, font=("times new roman", 20, "bold"), bg='white').grid(row=1, column=0, padx=20, pady=10)
-        ent_username =  Entry(login_frame, textvariable=self.username, bd=5, relief=GROOVE).grid(row=1, column=1, padx=10)
+        user_name_label = Label(login_frame, image=self.user_icon, text="Username", compound=LEFT, font=("times new roman", 20, "bold"), bg='white').grid(row=1, column=0, padx=20, pady=10)
+        ent_username = Entry(login_frame, textvariable=self.username, bd=5, relief=GROOVE).grid(row=1, column=1, padx=10)
 
-        password_label = Label(login_frame, text= "Password", compound=LEFT, font=("times new roman", 20, "bold"), bg='white').grid(row=2, column=0, padx=20, pady=10)
+        password_label = Label(login_frame, image=self.pass_icon, text= "Password", compound=LEFT, font=("times new roman", 20, "bold"), bg='white').grid(row=2, column=0, padx=20, pady=10)
         ent_password = Entry(login_frame, bd=5, relief=GROOVE).grid(row=2, column=1, padx=10)
 
         btn_register = Button(login_frame, text="Register", bg='red', fg='white', pady=5, width=10).grid(row=3, column=0, pady=10)
